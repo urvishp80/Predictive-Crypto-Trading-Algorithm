f972aafc9f12e729a72fb3ced861c97ba441f429
urvish <urvishp80@gmail.com>
2022-02-19 00:46:45 -0500

    Changes to app, added list of models, added actual model txt file

--- app.py
+++ app.py
@@ -1,29 +1,83 @@
-import json
-import requests
 import warnings
+import pandas as pd
 import os
-import datetime
-from flask import Flask, flash, request, jsonify, render_template
+from flask import Flask, request, jsonify, Response
 from glob import glob
 
 import config
 from src.models import LightGBMModel
 from predict import predict_single
+from prod_models_list import models_full_list
 
 # Ignore the warning
 warnings.simplefilter("ignore")
 
+app = Flask(__name__)
 
-def load_models():
+binary_score_models_dir = None
+trading_score_models_dir = None
+
+
+def load_binary_score_models():
+    """Loads the models for each of the objective and then create a dictionary
+
+    Returns:
+        dict: dictionary with models list as key.
+    """
     models_dir = {}
     model = LightGBMModel(config.model_parameters, config.fit_parameters, config, inference=False)
 
     for key, value in config.objectives_to_run.items():
         if value:
             obj_models = []
-            models_name_list = glob(os.path.join(config.PROD_MODELS_DIR, key))
+            models_name_list = models_full_list[key]["binary_score"]
             for i in models_name_list:
                 m = model.load(os.path.join(os.path.join(config.PROD_MODELS_DIR, key), i))
                 obj_models.append(m)
         models_dir[key] = obj_models
     return models_dir
+
+
+def load_trading_score_models():
+    """Loads the models for each of the objective and then create a dictionary
+
+    Returns:
+        dict: dictionary with models list as key.
+    """
+    models_dir = {}
+    model = LightGBMModel(config.model_parameters, config.fit_parameters, config, inference=False)
+
+    for key, value in config.objectives_to_run.items():
+        if value:
+            obj_models = []
+            models_name_list = models_full_list[key]["trading_score"]
+            for i in models_name_list:
+                m = model.load(os.path.join(os.path.join(config.PROD_MODELS_DIR, key), i))
+                obj_models.append(m)
+        models_dir[key] = obj_models
+    return models_dir
+
+
+# calling the load function as we need models loaded into memory as soon as the app starts.
+binary_score_models_dir = load_binary_score_models()
+trading_score_models_dir = load_trading_score_models()
+
+
+# get category of the app from play store
+@app.route('/api/predict', methods=['POST'])
+def getPredictions():
+    try:
+        data = request.json
+        if_binary = data["is_binary"]
+        df = pd.DataFrame.from_dict(data["data"], orient='table')
+        if if_binary:
+            preds = predict_single(df, binary_score_models_dir)
+        else:
+            preds = predict_single(df, trading_score_models_dir)
+        return preds
+    except Exception as ex:
+        return Response(f"An error occurred. {ex}.", status=400)
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
---
+++ prod_models_list.py
@@ -0,0 +1,20 @@
+models_full_list = {"obj8": {"binary_score": ["lgb_Obj 8 Linked_scale_pos_weight_5_20", "lgb_Obj 8 Linked_scale_pos_weight_6",
+                                              "lgb_Obj 6 Linked_scale_pos_weight_2_75", "lgb_Obj 6 Linked_scale_pos_weight_2_15.txt",
+                                              "lgb_Obj 6 Linked_scale_pos_weight_2_60.txt", "lgb_Obj 5 Linked_is_unbalanced.txt",
+                                              "lgb_Obj 5 Linked_scale_pos_weight_1_95.txt", "lgb_Obj 5 Linked_scale_pos_weight_2_00.txt",
+                                              "lgb_Obj 5 Linked_scale_pos_weigth_1_85.txt"],
+                             "trading_score": ["lgb_Obj 8 Linked_scale_pos_weight_5_20", "lgb_Obj 8 Linked_scale_pos_weight_6",
+                                               "lgb_Obj 6 Linked_scale_pos_weight_2_75", "lgb_Obj 5 Linked_is_unbalanced.txt",
+                                               "lgb_Obj 5 Linked_learning_rate_025_max_bin_1024.txt", "lgb_Obj 5 Linked_max_bin_512.txt",
+                                               "lgb_Obj 5 Linked_max_bin_768.txt", "lgb_Obj 5 Linked_scale_pos_weight_1_90.txt",
+                                               "lgb_Obj 5 Linked_scale_pos_weight_1_95.txt"]
+                             },
+                    "obj7": {"binary_score": ["lgb_Obj 8 Linked_scale_pos_weight_4_85.txt", "lgb_Obj 8 Linked_scale_pos_weight_5_15.txt",
+                                              "lgb_Obj 8 Linked_scale_pos_weight_5_95.txt", "lgb_Obj 7 Linked_scale_pos_weight_2_90",
+                                              "lgb3_Obj 7 Linked_sample_pos_w_2_85", "lgb_Obj 5 Linked_max_bin_1500_scale_pos_weight_2_45.txt",
+                                              "lgb_Obj 5 Linked_scale_pos_weight_2_00.txt", "lgb_Obj 5 Linked_scale_pos_weight_2_50.txt"],
+                             "trading_score": ["lgb_Obj 8 Linked_scale_pos_weight_6.txt", "lgb_Obj 8 Linked_scale_pos_weight_5_80.txt",
+                                               "lgb_Obj 8 Linked_scale_pos_weight_5_30.txt", "lgb_Obj 8 Linked_scale_pos_weight_5_20.txt",
+                                               "lgb_Obj 5 Linked_learning_rate_025_max_bin_1024.txt", "lgb_Obj 5 Linked_max_bin_768.txt"]
+                             }
+                    }
